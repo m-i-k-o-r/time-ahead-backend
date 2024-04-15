6aee88c4d5812cf14ebf4e34fd4be4115b421810
Korolitskiy Mihail <100139400+m-i-k-o-r@users.noreply.github.com>
2024-04-15 20:08:44 +0300

    tp-36 feature: add V0001__init_db.sql

---
+++ src/main/resources/db/migration/V0001__init_db.sql
@@ -0,0 +1,55 @@
+create table account
+(
+    id       uuid default gen_random_uuid() primary key,
+    email    text not null unique,
+    password text not null
+);
+
+create table category
+(
+    id      uuid default gen_random_uuid() primary key,
+    name    text not null,
+    user_id uuid not null,
+
+    foreign key (user_id) references account (id) on delete cascade
+);
+
+create table activity
+(
+    id          uuid default gen_random_uuid() primary key,
+    name        text      not null,
+    description text,
+    start_time  timestamp not null,
+    end_time    timestamp,
+    user_id     uuid      not null,
+    category_id uuid      not null,
+
+    foreign key (user_id) references account (id) on delete cascade,
+    foreign key (category_id) references category (id) on delete cascade
+);
+
+create table habit
+(
+    id              uuid default gen_random_uuid() primary key,
+    name            text    not null,
+    description     text,
+    repeat_reminder text    not null,
+    num_reminder    int     not null,
+    is_done         boolean not null,
+    user_id         uuid    not null,
+
+    foreign key (user_id) references account (id) on delete cascade
+);
+comment on column habit.repeat_reminder is 'Cron-выражениe';
+
+create table task
+(
+    id          uuid default gen_random_uuid() primary key,
+    name        text    not null,
+    description text,
+    reminder    timestamp,
+    is_done     boolean not null,
+    user_id     uuid    not null,
+
+    foreign key (user_id) references account (id) on delete cascade
+);
