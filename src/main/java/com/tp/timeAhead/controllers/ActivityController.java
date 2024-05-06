570adfde01a01b53c9afc1b990362a72fbf6e3ab
Korolitskiy Mihail <100139400+m-i-k-o-r@users.noreply.github.com>
2024-05-06 23:22:51 +0300

    tp-58 feature: add swagger annotations

--- src/main/java/com/tp/timeAhead/controllers/ActivityController.java
+++ src/main/java/com/tp/timeAhead/controllers/ActivityController.java
@@ -1,11 +1,14 @@
 package com.tp.timeAhead.controllers;
 
-import com.tp.timeAhead.data.responses.ActivityDto;
 import com.tp.timeAhead.data.requests.activity.ActivityCreateRequest;
 import com.tp.timeAhead.data.requests.activity.ActivityUpdateRequest;
+import com.tp.timeAhead.data.responses.ActivityDto;
 import com.tp.timeAhead.services.ActivityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -18,21 +21,55 @@ import java.util.UUID;
 public class ActivityController {
     private final ActivityService activityService;
 
+    @Operation(
+            summary = "Создать активность",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Активность создана")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping()
     public ActivityDto createActivity(@RequestBody ActivityCreateRequest form) {
         return activityService.createActivity(form);
     }
 
+    @Operation(
+            summary = "Изменить активность",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Активность изменена")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
     public ActivityDto updateActivity(@PathVariable UUID id, @RequestBody ActivityUpdateRequest form) {
         return activityService.updateActivity(id, form);
     }
 
+    @Operation(
+            summary = "Получить активность",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Активность получена")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public ActivityDto getActivity(@PathVariable UUID id) {
         return activityService.getActivity(id);
     }
 
+    @Operation(
+            summary = "Получить все активности",
+            description = "data - дата за которую требуется получить список выполненых активностей; " +
+                    "categoryId - категория по которой нужно найти все активности за этот день (data)",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Все активности получены")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping()
     public List<ActivityDto> getAllActivities(@RequestParam UUID userId,
                                               @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data,
@@ -40,6 +77,14 @@ public class ActivityController {
         return activityService.getAllActivity(userId, data, categoryId);
     }
 
+    @Operation(
+            summary = "Удалить активность",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Активность удалена")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
     public void deleteActivity(@PathVariable UUID id) {
         activityService.deleteActivity(id);
--- src/main/java/com/tp/timeAhead/controllers/CategoryController.java
+++ src/main/java/com/tp/timeAhead/controllers/CategoryController.java
@@ -1,10 +1,13 @@
 package com.tp.timeAhead.controllers;
 
-import com.tp.timeAhead.data.responses.CategoryDto;
 import com.tp.timeAhead.data.requests.category.CategoryCreateRequest;
 import com.tp.timeAhead.data.requests.category.CategoryUpdateRequest;
+import com.tp.timeAhead.data.responses.CategoryDto;
 import com.tp.timeAhead.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,26 +19,66 @@ import java.util.UUID;
 public class CategoryController {
     private final CategoryService categoryService;
 
+    @Operation(
+            summary = "Создать категорию",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Категория создана")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping()
     public CategoryDto createCategory(@RequestBody CategoryCreateRequest form) {
         return categoryService.createCategory(form);
     }
 
+    @Operation(
+            summary = "Изменить категорию",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Категория изменена")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
     public CategoryDto updateCategory(@PathVariable UUID id, @RequestBody CategoryUpdateRequest form) {
         return categoryService.updateCategory(id, form);
     }
 
+    @Operation(
+            summary = "Получить категорию",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Категория получена")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public CategoryDto getCategory(@PathVariable UUID id) {
         return categoryService.getCategory(id);
     }
 
+    @Operation(
+            summary = "Получить все категории",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Все категории получены")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping()
     public List<CategoryDto> getAllCategories(@RequestParam UUID userId) {
         return categoryService.getAllCategory(userId);
     }
 
+    @Operation(
+            summary = "Удалить категорию",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Категория удалена")
+            })
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable UUID id) {
         categoryService.deleteCategory(id);
