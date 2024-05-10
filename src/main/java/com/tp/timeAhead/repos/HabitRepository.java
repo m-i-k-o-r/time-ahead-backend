257d8c254e6399d7fbf6f46f6661833ec3c24e69
Korolitskiy Mihail <100139400+m-i-k-o-r@users.noreply.github.com>
2024-05-10 12:07:41 +0300

    tp-56 refactor: add new field habit

--- src/main/java/com/tp/timeAhead/repos/HabitRepository.java
+++ src/main/java/com/tp/timeAhead/repos/HabitRepository.java
@@ -1,21 +1,19 @@
 package com.tp.timeAhead.repos;
 
-import com.tp.timeAhead.models.Activity;
 import com.tp.timeAhead.models.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface HabitRepository extends JpaRepository<Habit, UUID> {
-    @Query("SELECT h FROM Habit h WHERE h.user.id = :userId AND LOWER(h.repeatReminder) LIKE CONCAT('%', LOWER(:day), '%')")
-    List<Habit> findAllByDay(@Param("userId") UUID userId, @Param("day") String day);
+    @Query("SELECT h FROM Habit h WHERE h.user.id = :userId AND h.isEnd = :isEnd AND LOWER(h.repeatReminder) LIKE CONCAT('%', LOWER(:day), '%')")
+    List<Habit> findAllByDay(@Param("userId") UUID userId, @Param("day") String day, @Param("isEnd") boolean isEnd);
 
-    @Query("SELECT h FROM Habit h WHERE h.user.id = :userId")
-    List<Habit> findAll(@Param("userId") UUID userId);
+    @Query("SELECT h FROM Habit h WHERE h.user.id = :userId AND h.isEnd = :isEnd")
+    List<Habit> findAll(@Param("userId") UUID userId, @Param("isEnd") boolean isEnd);
 }
